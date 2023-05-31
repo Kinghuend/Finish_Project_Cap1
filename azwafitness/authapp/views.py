2da9f5f37bbb61b20998db8984e453acc3d93954
Kinghuend <minh.huynh@madison-technology.com>
2023-05-31 18:56:19 +0700

    Thay Doi vi tri Trainer Trong view.py

--- azwafitness/authapp/views.py
+++ azwafitness/authapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
-from authapp.models import Contact,MembershipPlan,Trainer,Enrollment,Gallery,Attendance
+from authapp.models import Contact,MembershipPlan,Enrollment,Gallery,Attendance,Trainer
 from authapp.models import Suggest
 from authapp.forms import BotForm
 from authapp.models import Bot
